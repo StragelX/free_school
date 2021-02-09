f0ee5caedfef0141cbec6292e04243367000f337
StragelX <stragelx@gmail.com>
2021-02-09 22:37:39 +0200

    f

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,13 @@
   $( document ).ready(function() {
     const swiper = new Swiper('.swiper-container', {
-        loop: true,
-      });
+      loop: true,
+    });
+
+    $('.modal_overlay .close').click(function(){
+        $(this).closest(".modal_overlay").addClass("hidden");
+    })
+
+    $('.video_wrap .inner .one').click(function(){
+        $(".modal_overlay").removeClass("hidden");
+    })
 });
