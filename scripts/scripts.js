379f1dbebc7afc87dfd54a0d5b0b6875ee1ed5e3
StragelX <stragelx@gmail.com>
2021-02-10 10:27:56 +0200

    f

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -18,5 +18,13 @@
 
     $('.assigne_btn').click(function(){
       $(".form_overlay").removeClass("hidden");
-  })
+    })
+
+    $('.who_interested ul li').mouseover(function(){
+      $('.who_interested .img_wrap img').attr('src',$(this).attr('data-img'));
+    })
+
+    $('.our_values ul li').mouseover(function(){
+      $('.our_values .img_wrap img').attr('src',$(this).attr('data-img'));
+    })
 });
